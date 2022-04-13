24500dd1cee4ca7f0652124bcc8a69b96841c76a
Garv Choudhry <garvc07@Garvs-MacBook-Pro.local>
2022-04-13 14:16:56 -0400

    Made background and rocket image

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,27 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
+  // Declare image variables 
+  PImage imgBackground;
+  PImage imgRocket; 
+
+  // Declare size variables 
+  // Rocket
+  float fltRocketX = width / 2
+  float fltRocketY = 600;
+
+  // Shooting Star
+  float fltStarX;
+  float fltStarY;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1000, 700);
   }
 
   /** 
@@ -16,21 +29,30 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    
+    // Load background  
+    imgBackground = loadImage("space-background-illustration-for-explore-in-outer-space-vector.jpg");
+    
+    // Load Rocket
+    imgRocket = loadImage("red-rocket-png-5.png");
+    imgRocket.resize(imgRocket.width / 2, imgRocket.height / 2); // Resize rocket
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    // Apply background image and adjust position 
+    background(0);
+    image(imgBackground, -200, -200);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Draw rocket and animate 
+    image(imgRocket, fltRocketX, fltRocketY);
+    
+
+
+	
+ 
   }
-  
-  // define other methods down here.
 }
